8091e768f6d590e804f47ca41474857abdbe055b
NinoKrb <nino.gralla@gmx.de>
2022-05-15 15:35:33 +0200

    Create cursor class

---
+++ classes/cursor.py
@@ -0,0 +1,28 @@
+import pygame, os
+from settings import Settings
+
+class Cursor():
+    def __init__(self, filename):
+        self.action = "hand"
+        self.pos = (0,0)
+        pygame.mouse.set_visible(False)
+        self.update_sprite(filename)
+
+    def update(self):
+        self.pos = pygame.mouse.get_pos()
+        self.set_pos(*self.pos)
+
+    def set_pos(self, x, y):
+        self.rect.x, self.rect.y = x, y
+
+    def update_sprite(self, filename):
+        self.image = pygame.image.load(os.path.join(Settings.path_cursors, filename)).convert_alpha()
+        self.image = pygame.transform.scale(self.image, Settings.cursor_size)
+        self.rect = self.image.get_rect()
+
+    def draw(self, screen):
+        screen.blit(self.image, self.rect)
+
+    def get_pressed(self, buttons):
+        if pygame.mouse.get_pressed(num_buttons=3) == buttons:
+            return True
