7e186c3ff6f29f6ea954edffa95030190c87a54c
NinoKrb <nino.gralla@gmx.de>
2022-05-15 15:35:43 +0200

    Implement cursor class

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pygame, os, csv, json, random
 from settings import Settings
 from classes.player import Character
 from classes.timer import Timer
+from classes.cursor import Cursor
 from classes.fields import *
 
 class ImageLayer(pygame.sprite.Sprite):
@@ -17,32 +18,6 @@ class ImageLayer(pygame.sprite.Sprite):
 class CollisionLayer(ImageLayer):
     def __init__(self, filename):
         super().__init__(filename)
-
-class Cursor():
-    def __init__(self, filename):
-        self.action = "hand"
-        self.pos = (0,0)
-        pygame.mouse.set_visible(False)
-        self.update_sprite(filename)
-
-    def update(self):
-        self.pos = pygame.mouse.get_pos()
-        self.set_pos(*self.pos)
-
-    def set_pos(self, x, y):
-        self.rect.x, self.rect.y = x, y
-
-    def update_sprite(self, filename):
-        self.image = pygame.image.load(os.path.join(Settings.path_cursors, filename)).convert_alpha()
-        self.image = pygame.transform.scale(self.image, Settings.cursor_size)
-        self.rect = self.image.get_rect()
-
-    def draw(self, screen):
-        screen.blit(self.image, self.rect)
-
-    def get_pressed(self, buttons):
-        if pygame.mouse.get_pressed(num_buttons=3) == buttons:
-            return True
 
 
 class Game():
