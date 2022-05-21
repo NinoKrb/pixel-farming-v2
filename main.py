917736db67736ae977b8becb28840b0692e6ef6d
NinoKrb <nino.gralla@gmx.de>
2022-05-22 00:13:03 +0200

    Add zoom update

--- main.py
+++ main.py
@@ -7,28 +7,41 @@ from classes.fields import FieldManager
 from classes.item import ItemManager
 from classes.inventory import InventoryHandler, InventoryManager
 
+
 class ImageLayer(pygame.sprite.Sprite):
     def __init__(self, game, filename):
         super().__init__()
         self.game = game
+        self.original_image = self.update_sprite(filename).copy()
+
+    def update_sprite(self, filename):
         self.image = pygame.image.load(os.path.join(Settings.path_image, filename)).convert_alpha()
-        self.image = pygame.transform.scale(self.image, (int(Settings.window_width * self.game.zoom), int(Settings.window_height * self.game.zoom)))
+        self.update_zoom(self.image)
+        return self.image
+
+    def update_zoom(self, image):
+        self.image = pygame.transform.scale(image, (
+            int(Settings.window_width * self.game.zoom),
+            int(Settings.window_height * self.game.zoom)
+        ))
         self.rect = self.image.get_rect()
 
     def draw(self, screen):
         screen.blit(self.image, self.rect)
 
+
 class CollisionLayer(ImageLayer):
     def __init__(self, game, filename):
         super().__init__(game, filename)
 
+
 class Game():
     def __init__(self):
         super().__init__()
-        pygame.init()   
+        pygame.init()
         pygame.display.set_caption(Settings.title)
 
-        self.zoom = 1.5
+        self.zoom = Settings.zoom_default
         self.inventory_font = pygame.font.Font(os.path.join(Settings.path_font, "8-BIT WONDER.TTF"), 14)
 
         self.screen = pygame.display.set_mode((Settings.window_width, Settings.window_height))
@@ -56,7 +69,7 @@ class Game():
 
         self.inventory_state = False
 
-        self.running = True 
+        self.running = True
 
     def run(self):
         while self.running:
@@ -64,7 +77,7 @@ class Game():
             self.draw()
             self.update()
             self.watch_for_events()
-    
+
     def update(self):
         self.field_manager.update()
         self.cursor.update()
@@ -80,6 +93,11 @@ class Game():
         # self.character.draw(self.screen)
         pygame.display.flip()
 
+    def update_zoom(self):
+        self.background.update_zoom(self.background.original_image)
+        self.collision_layer.update_zoom(self.collision_layer.original_image)
+        self.field_manager.update_zoom()
+
     def watch_for_events(self):
         for event in pygame.event.get():
             if event.type == pygame.KEYDOWN:
@@ -91,3 +109,19 @@ class Game():
 
             if event.type == pygame.QUIT:
                 self.running = False
+
+            if event.type == pygame.MOUSEWHEEL:
+                new_zoom = self.zoom
+
+                # Zoom in
+                if event.y == 1:
+                    new_zoom = round(self.zoom + Settings.zoom_step, 1)
+
+                # Zoom Out
+                else:
+                    new_zoom = round(self.zoom - Settings.zoom_step, 1)
+
+                if Settings.zoom_max >= new_zoom >= Settings.zoom_min:
+                    self.zoom = new_zoom
+                    self.update_zoom()
+                print(self.zoom)
