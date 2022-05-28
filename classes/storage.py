088c12fd26ac6637aade3e31e0cfe576fcc8dfd2
NinoKrb <nino.gralla@gmx.de>
2022-05-29 01:56:06 +0200

    Create storage class

---
+++ classes/storage.py
@@ -0,0 +1,54 @@
+from settings import Settings
+import os, json
+
+
+class Storage():
+    def __init__(self, game):
+        self.game = game
+        self.save_game = "1"  # ID from the Save Game
+        self.filename = Settings.storage_filename
+        self.path = Settings.path_storage
+
+        self.storage = self.load_storage()
+        if not self.storage:
+            self.create_save_game(self.save_game)
+            self.save_storage(self.game)
+            self.storage = self.load_storage()
+
+    def create_save_game(self, save_game_id):
+        data, file = self.load_save_games()
+        new_save_game = {
+            "id": save_game_id,
+            "money": "",
+            "owned_fields": [],
+            "inventory": []
+        }
+        data['save_games'].append(new_save_game)
+        self.save_save_games(file, data)
+
+    def load_save_games(self):
+        file = open(os.path.join(self.path, self.filename), 'r+')
+        data = json.load(file)
+        return data, file
+
+    def save_save_games(self, file, data):
+        file.seek(0)
+        json.dump(data, file, indent=4)
+        file.truncate()
+
+    def load_storage(self):
+        data, file = self.load_save_games()
+        for scoreset in data['save_games']:
+            if scoreset['id'] == self.save_game:
+                return scoreset
+        return False
+
+    def save_storage(self, game):
+        data, file = self.load_save_games()
+        for scoreset in data['save_games']:
+            if scoreset['id'] == self.save_game:
+                scoreset['money'] = game.money
+                scoreset['owned_fields'] = game.owned_fields
+                scoreset['inventory'] = game.inventory.get_storage_inventory()
+
+        self.save_save_games(file, data)
