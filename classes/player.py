9ad9293822207acdb641d94aa8e141f208f3bf2c
NinoKrb <nino.gralla@gmx.de>
2022-05-28 21:17:38 +0200

    Add walking collisions & fix animations

--- classes/player.py
+++ classes/player.py
@@ -10,6 +10,7 @@ import pygame, os
 
 class Character(pygame.sprite.Sprite):
     def __init__(self, game, options, size, pos):
+        super().__init__()
         self.surface = pygame.Surface(size)
         self.game = game
         self.options = options
@@ -68,17 +69,18 @@ class Character(pygame.sprite.Sprite):
 class WalkingNPC(Character):
     def __init__(self, game, options, size, pos):
         super().__init__(game, options, size, pos)
-        self.walking_offset = 0
+        self.block_animations = False
         self.walking_speed = 0
-        self.old_pos = (0, 0)
+        self.old_animation_pos = (0, 0)
+        self.old_walking_pos = (0, 0)
         self.movement_timer = Timer(random.randint(1000, 2000))
         self.direction = random.choice(Settings.npc_directions)
         self.random_animation_timer = Timer(1000)
 
     def update_sprite(self, surface):
-        self.old_pos = self.pos
+        self.old_animation_pos = self.pos
         super().update_sprite(surface)
-        self.set_position(*self.old_pos)
+        self.set_position(*self.old_animation_pos)
 
     def calculate_movement(self, ignore_direction=None):
         if ignore_direction:
@@ -86,37 +88,27 @@ class WalkingNPC(Character):
                 self.direction = "down"
             elif ignore_direction == "down":
                 self.direction = "up"
-
-            if ignore_direction == "right":
-                self.direction = "left"
             elif ignore_direction == "left":
                 self.direction = "right"
+            elif ignore_direction == "right":
+                self.direction = "left"
 
         else:
-            self.direction = random.choice(Settings.npc_directions)  # Random walking direction choice
-
-        print(ignore_direction, self.direction)
-
+            self.direction = random.choice(Settings.npc_directions)     # Random walking direction choice
         self.walking_speed = random.randint(*Settings.npc_speed_range)  # Random Walking speed
 
-        if not ignore_direction:
-            # For diagonal walking offset
-            self.walking_offset = random.randrange(*Settings.npc_offset_range)
+        # Random Character actions
+        if abs(self.walking_speed) == 0:
+            if not self.block_animations:
+                self.action_handler.force_change_action(random.choice(['IDLE', 'WATERING', 'DOING']))
 
-            # Random Character actions
-            if sum([abs(self.walking_speed), abs(self.walking_offset)]) == 0:
-                self.action_handler.force_change_action(random.choice(['IDLE', 'DIG', 'AXE', 'HAMMERING', 'DOING']))
-                print(self.action_handler.current_action)
+        elif self.walking_speed == 1:
+            self.action_handler.force_change_action('WALKING')
 
-            elif sum([abs(self.walking_speed), abs(self.walking_offset)]) == 1:
-                self.action_handler.force_change_action('WALKING')
-                print(self.action_handler.current_action)
+        elif self.walking_speed == 2:
+            self.action_handler.force_change_action('RUN')
 
-            elif sum([abs(self.walking_speed), abs(self.walking_offset)]) == 2:
-                self.action_handler.force_change_action('RUN')
-                print(self.action_handler.current_action)
-
-    def movement(self):
+    def movement(self, is_collided=False):
         if self.movement_timer.is_next_stop_reached():
             self.calculate_movement()
 
@@ -132,26 +124,38 @@ class WalkingNPC(Character):
                 speed_x = self.walking_speed
 
             # Calculating if character needs to flip
-            if speed_x + self.walking_offset > 0:
+            if speed_x > 0:
                 self.flip = False
             else:
                 self.flip = True
 
-            if speed_x != 0:
-                self.rect.move_ip(speed_x, sum([speed_y, self.walking_offset]))
-            else:
-                self.rect.move_ip(sum([speed_x, self.walking_offset]), speed_y)
-            self.pos = (self.rect.x, self.rect.y)
-            self.set_position(*self.pos)
+            if not is_collided:
+                self.old_walking_pos = self.pos
+                self.rect.move_ip(speed_x, speed_y)
+                self.pos = (self.rect.x, self.rect.y)
 
     def check_collision(self):
-        if pygame.sprite.collide_mask(self, self.game.collision_layer):
-            return True
-        return False
+        hitlist = pygame.sprite.spritecollide(self, self.game.map_manager.border_tiles, False)
+        if len(hitlist) > 0:
+            for hit in hitlist:
+                if pygame.sprite.collide_mask(self, hit):
+                    return hitlist
+        else:
+            return False
+
+    def fix_position(self):
+        if self.direction != "up" or self.direction != "down":
+            self.flip = not self.flip
+
+        self.calculate_movement(self.direction)
+        self.set_position(*self.old_walking_pos)
+        self.pos = (self.rect.x, self.rect.y)
+        self.block_animations = True
 
     def update(self):
         super().update()
-        self.movement()
         if self.check_collision():
-            self.calculate_movement(self.direction)
-            self.set_position(*self.old_pos)
+            self.fix_position()
+        else:
+            self.movement()
+            self.block_animations = False
