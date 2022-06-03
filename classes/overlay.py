49b4d082575f8680b03af76c5a6ab13c5b5a7a05
NinoKrb <nino.gralla@gmx.de>
2022-06-03 23:46:38 +0200

    Add fadeout for seed info

--- classes/overlay.py
+++ classes/overlay.py
@@ -1,5 +1,6 @@
 import pygame, os
 from settings import Settings
+from classes.timer import Timer
 
 
 class OverlaySprite(pygame.sprite.Sprite):
@@ -30,6 +31,7 @@ class OverlaySprite(pygame.sprite.Sprite):
 
 class OverlayManager():
     def __init__(self, game):
+        self.seed_label_alpha = None
         self.amount_label = None
         self.seed_label = None
         self.money_label = None
@@ -41,6 +43,9 @@ class OverlayManager():
 
         self.current_action_item = OverlaySprite('hand_open_02.png', (24, Settings.window_height - 72), (48, 48), Settings.path_cursors)
 
+        self.alpha_timer = Timer(Settings.overlay_alpha_timer)
+        self.reset_alpha("seed_label")
+
     def draw(self, screen):
         self.sprites.draw(screen)
         self.current_action_item.draw(screen)
@@ -49,11 +54,21 @@ class OverlayManager():
         screen.blit(self.money_label, (Settings.window_width - 110, 26))
 
         if self.game.action['name'] == "seed":
+            if self.alpha_timer.is_next_stop_reached():
+                if self.seed_label_alpha > 0:
+                    self.seed_label_alpha -= Settings.overlay_alpha_step
+
             self.seed_label = self.game.inventory_font_big.render(f"{self.game.inventory.find_item(self.game.current_seed['item_id']).item.name}", True, (255, 255, 255))
+            self.seed_label.set_alpha(self.seed_label_alpha)
             screen.blit(self.seed_label, (self.current_action_item.rect.right + 25, self.current_action_item.rect.top))
 
             self.amount_label = self.game.inventory_font.render(f"{self.game.inventory.get_amount_by_id(self.game.current_seed['item_id'])}", True, (255, 255, 255))
+            self.amount_label.set_alpha(self.seed_label_alpha)
             screen.blit(self.amount_label, (self.current_action_item.rect.right + 25, self.current_action_item.rect.top + self.seed_label.get_rect().height + 5))
 
     def update(self):
         pass
+
+    def reset_alpha(self, object):
+        if object == "seed_label":
+            self.seed_label_alpha = Settings.overlay_alpha_max
